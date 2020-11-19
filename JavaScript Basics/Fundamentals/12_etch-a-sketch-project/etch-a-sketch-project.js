e550989373844f0d85c42cd33359522baf835edd
RodrigoSS <69469262+RoySS248@users.noreply.github.com>
2020-11-19 13:42:37 -0600

    update visual etch

--- JavaScript Basics/Fundamentals/12_etch-a-sketch-project/etch-a-sketch-project.js
+++ JavaScript Basics/Fundamentals/12_etch-a-sketch-project/etch-a-sketch-project.js
@@ -1,5 +1,6 @@
 const gridContainer = document.querySelector('#grid-container');
 
+let color = false;
 createGrid(16);
 
 function createGrid(size) {
@@ -10,9 +11,9 @@ function createGrid(size) {
 
     const grid = document.createElement('div');
     grid.classList.add('box');
-    grid.id = i+1;
+    grid.id = i + 1;
     /*grid.textContent = i+1;*/
-    grid.style.padding = ((80/(size*2))-0.1)+"vh";
+    grid.style.padding = ((80 / (size * 2)) - 0.1) + "vh";
 
     gridContainer.appendChild(grid);
     i++
@@ -20,7 +21,7 @@ function createGrid(size) {
 }
 
 
-function clearGrid(){
+function clearGrid() {
 
   let i = 0;
   while (i !== gridContainer.children.length) {
@@ -28,18 +29,18 @@ function clearGrid(){
     gridContainer.removeChild(box);
   }
 
-  
-  let size = prompt("Tamaño (max 100): ",16);
 
-  if( isNaN(size*1) ){
+  let size = prompt("Tamaño (max 100): ", 16);
+
+  if (isNaN(size * 1)) {
     size = 16;
   }
 
-  if(size > 100){
+  if (size > 100) {
     size = 100;
   }
 
-  if(size < 2){
+  if (size < 2) {
     size = 2;
   }
 
@@ -48,9 +49,42 @@ function clearGrid(){
 }
 
 
+
+function randomColor() {
+  let colorSelection = Math.floor((Math.random() * 5) + 1);
+  switch (colorSelection) {
+    case 1:
+      return "red";
+      break;
+    case 2:
+      return "green";
+      break;
+    case 3:
+      return "blue";
+      break;
+    case 4:
+      return "yellow";
+      break;
+    case 5:
+      return "purple";
+      break;
+  }
+}
+
+
 const btnClear = document.querySelector("#btn-clear");
 btnClear.addEventListener("click", () => {
   clearGrid();
+});
+
+const btnColor = document.querySelector("#btn-color");
+btnColor.addEventListener("click", () => {
+  color = true;
+});
+
+const btnBlack = document.querySelector("#btn-black");
+btnBlack.addEventListener("click", () => {
+  color = false;
 });
 
 /*------------------------------------------- */
@@ -58,8 +92,14 @@ btnClear.addEventListener("click", () => {
 gridContainer.addEventListener('mouseover', function (e) {
 
   if (e.target.id != "grid-container") {
-    e.target.style.background = "black";
+
+    if(color){
+      e.target.style.background = randomColor();
+    }else{
+      e.target.style.background = "black";
+    }
+
   }
 
 
-}); 
+});
