bc5fed132fa7cf00b6d637242adeabf382700cc8
RodrigoSS <69469262+RoySS248@users.noreply.github.com>
2020-11-24 13:15:37 -0600

    palindromos

--- JavaScript Basics/Fundamentals/13_Test_Driven_Development_2/palindromes/palindromes.js
+++ JavaScript Basics/Fundamentals/13_Test_Driven_Development_2/palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function() {
+const palindromes = function(word) {
+  newWord = word.toLowerCase().replace(/[^A-Za-z]/g, "");
+  arrayWord = newWord.split("")
+  arrayWord = arrayWord.reverse();
+  if(arrayWord.join("") == newWord){
+    return true;
+  }else{
+    return false;
+  }
 
-}
+};
 
-module.exports = palindromes
+module.exports = palindromes;
