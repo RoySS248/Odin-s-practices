7dbfe9a68863462247db1c47726c8d7dd1fc467f
RodrigoSS <69469262+RoySS248@users.noreply.github.com>
2021-03-11 14:04:39 -0600

    to_do_list_update

--- JavaScript/4_To_do_list/todo.js
+++ JavaScript/4_To_do_list/todo.js
@@ -6,9 +6,8 @@ let currentProject = 0;
 
 //     Proyecto
 const createProject = (name) => {
-
   const tasks = [
-    ["NAME", "DESCRIPTION", "DATE", "PRIORITY"]
+//    ["NAME", "DESCRIPTION", "DATE", "PRIORITY"]
   ];
 
   const getName = () => name;
@@ -22,7 +21,6 @@ const createProject = (name) => {
 
 
 const DOM_projects = (() => {
-
   const resetProjects = () => {
     while (containerProject.firstChild) {
       containerProject.removeChild(containerProject.firstChild);
@@ -31,15 +29,14 @@ const DOM_projects = (() => {
 
   const drawProjects = () => {
     resetProjects();
-
     projectsArray.forEach((project, i) => {
       let cardProject = document.createElement("div");
       cardProject.classList.add("card-project");
       cardProject.id = i;
+      cardProject.textContent = projectsArray[i].getName();
       cardProject.onclick = () => {
         getIndexProject(cardProject.id)
       };
-      cardProject.textContent = projectsArray[i].getName();
       containerProject.appendChild(cardProject);
     });
   }
@@ -51,7 +48,13 @@ const DOM_projects = (() => {
 
 
 function getIndexProject(id = 0) {
+
+  let selects = document.getElementsByClassName("card-project-select");
+  for (let i = 0; i < selects.length; i++) {
+    selects[i].classList.remove("card-project-select");
+  }
   currentProject = id;
+  document.getElementById(id).classList.add("card-project-select");
   DOM_Tasks.drawTasks(projectsArray[id].getName(), projectsArray[id].getTasks(), id);
 }
 
@@ -75,8 +78,6 @@ const DOM_Tasks = (() => {
     resetTasks();
 
     currentTasks.forEach((task, i) => {
-      console.log(task);
-
       let cardTask = document.createElement("div");
       cardTask.classList.add("card-task");
       cardTask.id = id;
@@ -114,7 +115,7 @@ const DOM_Tasks = (() => {
 const defa = createProject("defa");
 projectsArray.push(defa);
 DOM_projects.drawProjects();
-getIndexProject();
+getIndexProject(currentProject);
 
 
 addProject = () => {
@@ -126,6 +127,7 @@ addProject = () => {
   console.log(projectsArray);
 
   DOM_projects.drawProjects();
+  getIndexProject(currentProject);
   
 }
 
@@ -135,9 +137,15 @@ addTasks = () => {
   let dateTask = document.getElementById("date-task").value;
   let priorityTask = document.getElementById("priority-task").value;
 
+
+  document.getElementById("name-task").value = "";
+  document.getElementById("description-task").value = "";
+  document.getElementById("date-task").value = "";
+  document.getElementById("priority-task").value = "Low" ;
+
   nameTask = createTask(nameTask, descriptionTask, dateTask, priorityTask, currentProject);
 
   DOM_projects.drawProjects();
-  getIndexProject()
+  getIndexProject(currentProject);
 
 }
